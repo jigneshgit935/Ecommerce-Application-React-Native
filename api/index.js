1bd5b18dccd7e1040d128845a3f152d25a396eca
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-07 04:38:24 +0530

    api-created to add address of a particular user

--- api/index.js
+++ api/index.js
@@ -158,3 +158,42 @@ app.post('/login', async (req, res) => {
 });
 
 // endpoint to store a new address into the backend
+app.post('/addresses', async (req, res) => {
+  try {
+    const { userId, address } = req.body;
+
+    // find the user by userid
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // add the new address to the users addresses array
+    user.addresses.push(address);
+
+    // save the updated user in the backend
+    await user.save();
+
+    res.status(200).json({ message: 'Address Created Successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error Entering Address' });
+  }
+});
+
+//endpoint to get all the addresses of a particular user
+app.get('/addresses/:userId', async () => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const addresses = user.addresses;
+    res.status(200).json({ addresses });
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving the address' });
+  }
+});
