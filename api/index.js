abf5cbfbab64ff3673fadebfc0731511c7b80a2a
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-08 06:27:28 +0530

    api created to store products, to get profile of u

--- api/index.js
+++ api/index.js
@@ -198,4 +198,73 @@ app.get('/addresses/:userId', async (req, res) => {
   }
 });
 
-// 5:52:00
+//endpoint to store all the orders
+app.post('/orders', async (req, res) => {
+  try {
+    const { userId, cartItems, totalPrice, shippingAddress, paymentMethod } =
+      req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    //create an array of product objects from the cart Items
+    const products = cartItems.map((item) => ({
+      name: item?.title,
+      quantity: item.quantity,
+      price: item.price,
+      image: item?.image,
+    }));
+
+    //create a new Order
+    const order = new Order({
+      user: userId,
+      products: products,
+      totalPrice: totalPrice,
+      shippingAddress: shippingAddress,
+      paymentMethod: paymentMethod,
+    });
+
+    await order.save();
+
+    res.status(200).json({ message: 'Order created successfully!' });
+  } catch (error) {
+    console.log('error creating orders', error);
+    res.status(500).json({ message: 'Error creating orders' });
+  }
+});
+
+//get the user profile
+app.get('/profile/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving the user profile' });
+  }
+});
+
+// get the order of that particular user
+app.get('/orders/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const orders = await Order.find({ user: userId }).populate('user');
+
+    if (!orders || orders.length === 0) {
+      return res.status(404).json({ message: 'No orders found for this user' });
+    }
+
+    res.status(200).json({ orders });
+  } catch (error) {
+    res.status(500).json({ message: 'Error' });
+  }
+});
